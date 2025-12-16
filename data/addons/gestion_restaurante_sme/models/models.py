6a5caad075ea026c941ee29b44fd8521d1bc73b6
Sergi <sermiresc@alu.edu.gva.es>
2025-12-16 09:41:03 +0100

    Practica 3

--- data/addons/gestion_restaurante_sme/models/models.py
+++ data/addons/gestion_restaurante_sme/models/models.py
@@ -1,17 +1,113 @@
 from odoo import models, fields, api
 
 
-class plato_sergi(models.Model):
-    _name = 'gestion_restaurante_sme.gestion_restaurante_sme'
-    _description = 'gestion_restaurante_sme.gestion_restaurante_sme'
+class plato_sme(models.Model):
+    _name = 'gestion_restaurante_sme.plato_sme'
+    _description = 'gestion_restaurante_sme.plato_sme'
 
-    name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-    description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    name = fields.Char(
+        string="Nombre",
+        required=True,
+        help="Introduzca el nombre del plato")
+    description = fields.Text(
+        string="Descripción",
+        help="Breve descripción del plato")
 
+    precio = fields.Float(
+        string="Precio",
+        required=True,
+        help="Precio del plato")
+
+    tiempo_preparacion = fields.Integer(
+        string="Tiempo de preparación",
+        help="Tiempo de preparación en minutos")
+
+    disponible = fields.Boolean(
+        string="Disponible",
+        default=True)
+
+    categoria = fields.Selection([
+        ("entrante", "Entrante"),
+        ("principal", "Principal"),
+        ("postre", "Postre"),
+        ("bebida", "Bebida")
+    ],
+    string="Categoria",
+    help="Categoria del plato")
+
+    menu = fields.Many2one(
+        'gestion_restaurante_sme.menu_sme',
+        string="Menu",
+        ondelete="set null",
+        help="Menu del plato")
+
+    rel_ingredientes = fields.Many2many(
+        comodel_name="gestion_restaurante_sme.ingrediente_sme",
+        relation="relacion_platos_ingredientes",
+        column1="rel_platos",
+        column2="rel_ingredientes",
+        string="Ingredients",
+        help="Ingredientes del plato")
+
+class menu_sme(models.Model):
+    _name = 'gestion_restaurante_sme.menu_sme'
+    _description = 'gestion_restaurante_sme.menu_sme'
+
+    name = fields.Char(
+        string="Nombre",
+        required=True,
+        help="Introduzca el nombre del Menu")
+    description = fields.Text(
+        string="Descripción",
+        help="Breve descripción del Menu")
+
+    fecha_inicio = fields.Date(
+        string="Fecha inicio",
+        help="Fecha de inicio del menu",
+        required=True)
+
+    fecha_fin = fields.Date(
+        string="Fecha fin",
+        help="Fecha de fin del menu")
+
+    activo = fields.Boolean(
+        string="Activo",
+        help="Si el menu esta activo o no")
+
+    platos = fields.One2many(
+        'gestion_restaurante_sme.plato_sme',
+        'menu',
+        string="Platos",
+        help="Platos incluidos en el menu")
+
+    
+class ingrediente_sme(models.Model):
+    _name = 'gestion_restaurante_sme.ingrediente_sme'
+    _description = 'gestion_restaurante_sme.ingrediente_sme'
+
+    name = fields.Char(
+        string="Nombre",
+        help="Nombre del ingrediente",
+        required=True)
+
+    es_alergeno = fields.Boolean(
+        string="Alergeno",
+        help="indica si el ingrediente es alergeno")
+
+
+    description = fields.Text(
+        string="Descripción",
+        help="Descripción del ingrediente")
+
+    rel_platos = fields.Many2many(
+        comodel_name="gestion_restaurante_sme.plato_sme",
+        relation="relacion_platos_ingredientes",
+        column1="rel_ingredientes",
+        column2="rel_platos",
+        string="Plato",
+        help="platos que contienen el ingrediente")
+
+    
+
+
+    
--- data/addons/gestion_tareas_sme/models/models.py
+++ data/addons/gestion_tareas_sme/models/models.py
@@ -5,30 +5,30 @@ class gestion_tareas_sme(models.Model):
     _name = 'gestion_tareas_sme.gestion_tareas_sme'
     _description = 'gestion_tareas_sme.gestion_tareas_sme'
 
-    nombre = fields.Char(
+    name = fields.Char(
         string="Nombre", 
         required=True, 
         help="Introduzca el nombre de la tarea")
 
-    descripcion = fields.Text(
+    description = fields.Text(
         string="Descripción", 
         help="Breve descripción de la tarea")
 
-    fecha_creacion = fields.Date(
+    date_creacion = fields.Date(
         string="Fecha Creación", 
         required=True, 
         help="Fecha en la que se dio de alta la tarea")
 
-    fecha_ini = fields.Datetime(
+    date_ini = fields.Datetime(
         string="Fecha Inicio", 
         required=True, 
         help="Fecha y hora de inicio de la tarea")
 
-    fecha_fin = fields.Datetime(
+    date_fin = fields.Datetime(
         string="Fecha Final", 
         help="Fecha y hora de finalización de la tarea")
 
-    finalizado = fields.Boolean(
+    ended = fields.Boolean(
         string="Finalizado", 
         help="Indica si la tarea ha sido finalizada o no")
 
@@ -37,6 +37,13 @@ class gestion_tareas_sme(models.Model):
         string='Sprint relacionado', 
         ondelete='set null', 
         help='Sprint al que pertenece esta tarea')
+
+    rel_tecnologias = fields.Many2many(
+        comodel_name='gestion_tareas_sme.tecnologias_sme',
+        relation='relacion_tareas_tecnologias',
+        column1='rel_tareas',
+        column2='rel_tecnologias',
+        string='Tecnologías')
 
 class sprints_sme(models.Model):
     _name = 'gestion_tareas_sme.sprints_sme'
@@ -64,6 +71,32 @@ class sprints_sme(models.Model):
         'gestion_tareas_sme.gestion_tareas_sme', 
         'sprint', 
         string='Tareas del Sprint')
+
+class tecnologias_sme(models.Model):
+    _name = 'gestion_tareas_sme.tecnologias_sme'
+    _description = 'Modelo de Tecnologías'
+
+    name = fields.Char(
+        string="Nombre", 
+        required=True, 
+        help="Nombre de la tecnología")
+
+    description = fields.Text(
+        string="Descripción", 
+        help="Breve descripción de la tecnología")
+
+    logo = fields.Image(
+        string="Logo", 
+        max_width=256, 
+        max_height=256,
+        help="Logo de la tecnología")
+
+    rel_tareas = fields.Many2many(
+        comodel_name='gestion_tareas_sme.gestion_tareas_sme',
+        relation='relacion_tareas_tecnologias',
+        column1='rel_tecnologias',
+        column2='rel_tareas',
+        string='Tareas')
 
         
 
